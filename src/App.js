28e01977a1a90e2e4d562ebf3b54c05be53e68df
bhabinpun <poon.bhabin6332@gmail.com>
2024-06-10 15:37:09 +0545

    adding projects component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Experiences from "./components/Experiences";
 import Services from "./components/Services";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Services />
       <Experiences />
+      <Projects />
     </>
   );
 }
