10dc0e1d300ff942ed1370e060ef1b66a32c31c1
Bhabin <poon.bhabin6332@gmail.com>
2025-01-10 23:04:35 +0545

    start to write logic part or function

--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -7,11 +7,28 @@ function TodoList() {
     { id: 2, title: "todo 2", completed: true },
   ]);
 
+  const [text, setText] = React.useState("");
+  function addTodo(text) {
+    const newTodo = {
+      id: Date.now(),
+      title: text,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+    setText("");
+  }
+
   return (
     <div>
       {todos.map((todo) => (
         <TodoItem key={todo.id} todo={todo} />
       ))}
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={() => addTodo(text)}>Add</button>
     </div>
   );
 }
