acd867f32dae1f785dbb7f0038ca950d8ea9f7fe
Bhabin <poon.bhabin6332@gmail.com>
2025-01-13 22:48:44 +0545

    add to-do app functionality add, delete and toggle to complete

--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import TodoItem from "./TodoItem";
 
 function TodoList() {
+  
+  //Demo Values
   const [todos, setTodos] = React.useState([
     { id: 1, title: "todo 1", completed: false },
     { id: 2, title: "todo 2", completed: true },
   ]);
 
   const [text, setText] = React.useState("");
+
   function addTodo(text) {
     const newTodo = {
       id: Date.now(),
@@ -18,10 +21,31 @@ function TodoList() {
     setText("");
   }
 
+  function deleteTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function toggleCompleted(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        } else {
+          return todo;
+        }
+      })
+    );
+  }
+
   return (
     <div>
       {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
+        <TodoItem
+          key={todo.id}
+          deleteTodo={deleteTodo}
+          toggleCompleted={toggleCompleted}
+          todo={todo}
+        />
       ))}
       <input
         type="text"
