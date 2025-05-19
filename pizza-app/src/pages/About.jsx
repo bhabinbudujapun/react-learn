253aeeb6aa4f7e237a3bbe1778bdc99e0c662d9b
Bhabin <poon.bhabin6332@gmail.com>
2025-05-19 22:21:50 +0545

    src: add components and pages of pizza app.

---
+++ pizza-app/src/pages/About.jsx
@@ -0,0 +1,9 @@
+const about = () => {
+  return (
+    <>
+      <h1>This is the About page !!</h1>
+    </>
+  );
+};
+
+export default about;
---
+++ pizza-app/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <div className="hero py-16">
+      <div>
+        <div className="w-1/2">
+          <h1>
+            <em>Are you hungry?</em>
+          </h1>
+          <h6>Don't wait</h6>
+        </div>
+        <div className="W-1/2">
+          <img src="../../images/pizza.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
