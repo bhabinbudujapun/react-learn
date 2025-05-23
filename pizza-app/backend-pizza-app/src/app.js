dc9c2084181ed6047b53cc1cac73414acdd7ab77
Bhabin <poon.bhabin6332@gmail.com>
2025-05-23 23:46:56 +0545

    update(backend): add new route for single and all product, made availabel images from frontend

--- pizza-app/backend-pizza-app/src/app.js
+++ pizza-app/backend-pizza-app/src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-
 const app = express();
 
 app.use(
@@ -8,76 +7,63 @@ app.use(
     origin: "http://localhost:5173",
   })
 );
+
+app.use("/assets", express.static("public"));
+
 const products = [
   {
-    _id: {
-      $oid: "5eee651f739f8c674fd736ee",
-    },
+    $_id: "5eee651f739f8c674fd736ee",
     name: "Margherita",
-    image: "D:GitHub\react-learnpizza-app\backend-pizza-apppublicimages",
+    image: "Margherita.png",
     price: "250",
     size: "small",
   },
   {
-    _id: {
-      $oid: "5eee6671a27a66807cf2bea3",
-    },
+    $_id: "5eee6671a27a66807cf2bea3",
     name: "Marinara",
-    image: "pizza.png",
+    image: "Chicken_Mushroom.webp",
     price: "300",
     size: "medium",
   },
   {
-    _id: {
-      $oid: "5eee6692a27a66807cf2bea4",
-    },
+    $_id: "5eee6692a27a66807cf2bea4",
     name: "Carbonara",
-    image: "pizza.png",
+    image: "Vegies.jpg",
     price: "200",
     size: "small",
   },
   {
-    _id: {
-      $oid: "5eee66a5a27a66807cf2bea5",
-    },
+    $_id: "5eee66a5a27a66807cf2bea5",
     name: "Americana",
-    image: "pizza.png",
+    image: "Paneer.webp",
     price: "500",
     size: "large",
   },
   {
-    _id: {
-      $oid: "5eee66c4a27a66807cf2bea6",
-    },
+    $_id: "5eee66c4a27a66807cf2bea6",
     name: "Chicken Mushroom",
-    image: "pizza.png",
+    image: "Chicken_Mushroom.webp",
     price: "350",
     size: "medium",
   },
   {
-    _id: {
-      $oid: "5eee66cfa27a66807cf2bea7",
-    },
+    $_id: "5eee66cfa27a66807cf2bea7",
     name: "Paneer pizza",
-    image: "pizza.png",
+    image: "Vegies.jpg",
     price: "200",
     size: "small",
   },
   {
-    _id: {
-      $oid: "5eee66eea27a66807cf2bea8",
-    },
+    $_id: "5eee66eea27a66807cf2bea8",
     name: "Vegies pizza",
-    image: "pizza.png",
+    image: "Margherita.png",
     price: "600",
     size: "large",
   },
   {
-    _id: {
-      $oid: "5eee6717a27a66807cf2bea9",
-    },
+    $_id: "5eee6717a27a66807cf2bea9",
     name: "Pepperoni",
-    image: "pizza.png",
+    image: "Chicken_Mushroom.webp",
     price: "500",
     size: "medium",
   },
@@ -88,8 +74,33 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/products", (req, res) => {
+  const baseUrl = "http://localhost:8000/assets/images/";
+
+  const updatedProducts = products.map((product) => ({
+    ...product,
+    image: `${baseUrl}${product.image}`,
+  }));
+
   res.setHeader("Content-Type", "application/json");
-  res.json({ products });
+  res.json({ products: updatedProducts });
+});
+
+app.get("/api/product/:_id", (req, res) => {
+  const { _id } = req.params;
+  const product = products.find((p) => p.$_id === _id);
+
+  if (!product) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+
+  const baseUrl = "http://localhost:8000/assets/images/";
+
+  const updatedProducts = {
+    ...product,
+    image: `${baseUrl}${product.image}`,
+  };
+
+  res.json(updatedProducts);
 });
 
 export default app;
