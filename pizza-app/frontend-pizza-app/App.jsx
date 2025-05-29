3896569e065a1943958516acb5b5888c75747df1
Bhabin <poon.bhabin6332@gmail.com>
2025-05-29 23:41:44 +0545

    add: add routes, state for cart and import contextAPI

--- pizza-app/frontend-pizza-app/App.jsx
+++ pizza-app/frontend-pizza-app/App.jsx
@@ -6,19 +6,36 @@ import AllProduct from "./src/pages/AllProduct.jsx";
 import SingleProduct from "./src/pages/SingleProduct.jsx";
 import Navigation from "./src/components/Navigation.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import CartContext from "./src/context/CartContext.jsx";
+import { getCart, storeCart } from "./src/helpers/CartUtils.jsx";
 
 const App = () => {
+  const [cart, setCart] = useState({});
+
+  useEffect(() => {
+    getCart().then((cart) => {
+      setCart(JSON.parse(cart));
+    });
+  }, []);
+
+  useEffect(() => {
+    storeCart(JSON.stringify(cart));
+  }, [cart]);
+
   return (
     <>
       <Router>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<AllProduct />} />
-          <Route path="/products/:_id" element={<SingleProduct />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
+        <CartContext.Provider value={{ cart, setCart }}>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/products" element={<AllProduct />} />
+            <Route path="/products/:_id" element={<SingleProduct />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </CartContext.Provider>
       </Router>
     </>
   );
