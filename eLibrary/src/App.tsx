cd699097f8799894aa2c6409d07f38011b5022df
Bhabin <poon.bhabin6332@gmail.com>
2025-06-05 21:40:11 +0545

    initial-commit: start project eLibrary frontend portion, mostly on dashboard and authentication

---
+++ eLibrary/src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { Calendar } from "@/components/ui/calendar";
+
+const App = () => {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
+  return (
+    <>
+      <div className="flex flex-col flex-wrap items-start gap-2 @md:flex-row">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-md border shadow-sm"
+        />
+      </div>
+    </>
+  );
+};
+
+export default App;
