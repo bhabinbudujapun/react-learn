9d08e8c2eff003d8cd900f0c67b5c8ea701379ec
bhabinpun <poon.bhabin6332@gmail.com>
2025-06-08 20:48:14 +0545

    update: set new route

--- eLibrary/src/main.tsx
+++ eLibrary/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import router from "./router.tsx";
+import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}></RouterProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
--- eLibrary/src/router.tsx
+++ eLibrary/src/router.tsx
@@ -5,15 +5,20 @@ import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import BooksPage from "./pages/BooksPage";
 import AuthLayout from "./layout/AuthLayout";
+import DashboardPage from "./pages/DashboardPage";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
   {
     path: "dashboard",
     element: <DashboardLayout />,
     children: [
       {
         path: "home",
-        element: <HomePage />,
+        element: <DashboardPage />,
       },
 
       {
