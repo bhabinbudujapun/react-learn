d7badceb4da935324337dbed99011818fb1a96c1
bhabinpun <poon.bhabin6332@gmail.com>
2025-06-14 20:49:40 +0545

    feat: add getBooks API utility function

--- eLibrary/src/http/api.ts
+++ eLibrary/src/http/api.ts
@@ -1,17 +1,25 @@
+import type { Book } from "@/types";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-export const login = async (data: { email: string; password: string }) =>
-  api.post("/api/users/login", data);
+export const login = async (data: { email: string; password: string }) => {
+  const res = api.post("/api/users/login", data);
+  return res;
+};
 
 export const register = async (data: {
   name: string;
   email: string;
   password: string;
 }) => api.post("/api/users/register", data);
+
+export const getBooks = async () => {
+  const res = await api.get<Book[]>("/api/books");
+  return res;
+};
